e986615844675942dfb48297e7d727d6848955b5
anish2210 <anishjaiswal1220@gmail.com>
2024-01-12 10:13:17 +0530

    sending querey

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-app.use(bodyParser.json());
+function sum(n){
+  let ans = 0;
+  for(let i=0; i<=n; i++){
+    ans += i;
+  }
+  return ans;
+}
 
-app.post('/', (req, res) => {
-  console.log(req.body);
-  res.send("hello world")
+// app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  // console.log(req.body);
+  const n = req.query.n;
+  const ans = sum(n);
+  res.send("hello world your asn is "+ans);
 })
 
 app.listen(port, () => {
