8e4264b54b86a0ed718ef9148f98f538157bd604
anish2210 <anishjaiswal1220@gmail.com>
2024-03-12 16:20:48 +0530

    react app

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+// state, component
+
+function App() {
+  const [count, setCount] = useState(0);
+
+return (
+  <div>
+      <CustomButton count = {count} setCount={setCount}></CustomButton>
+    </div>
+  );
+}
+
+function CustomButton(props){
+  
+  function onClickHandler(){
+    props.setCount(props.count + 1);
+  }
+    
+  return <button onClick={onClickHandler}>
+      Counter {props.count}
+    </button>
+  }
+    
+  export default App;
