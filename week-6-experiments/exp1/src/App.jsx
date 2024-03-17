9414e022cec9ed875077b6f4fb3a95af9f5ad127
anish2210 <anishjaiswal1220@gmail.com>
2024-03-18 01:03:09 +0530

    cont

--- week-6-experiments/exp1/src/App.jsx
+++ week-6-experiments/exp1/src/App.jsx
@@ -1,29 +1,33 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
-  const [title, setTitle] = useState("My name is Anish Jaiswal")
-
-  function updateTitle(){
-     setTitle("my name is "+ Math.random()); 
-  }
-
   return (
     <div>
+      <HeaderWithButtons></HeaderWithButtons>
+      <Header title="anish2" />
+      <Header title="anish3" />
+    </div>
+  );
+}
+
+function HeaderWithButtons() {
+  const [title, setTitle] = useState("My name is Anish Jaiswal");
+  
+  function updateTitle() {
+    setTitle("my name is " + Math.random());
+  }
+  
+  return (
+    <>
       <button onClick={updateTitle}>Click me to change the title</button>
       <Header title={title}></Header>
-      <Header title="anish2"/>
-      <Header title="anish3"/>
-      <Header title="anish4"/>
-      <Header title="anish5"/>
-      <Header title="anish6"/>
-    </div>
-  )
+    </>
+  );
 }
 
-function Header({title}){
-  return <div>
-    {title}
-  </div>
+
+function Header({ title }) {
+  return <div>{title}</div>;
 }
 
-export default App
+export default App;
